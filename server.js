5a99acde8f3715c97fb04406d9ceb234ec04e14d
Cindy Unrau <cindy4th@hotmail.com>
2022-08-11 12:19:45 -0700

    server test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./src/routes/creature.routes"));
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to cindy's application." });
+});
+
 const db = require("./src/db/conn.js");
 const port = process.env.PORT || 8081
 app.listen(port, () => {
@@ -15,6 +19,4 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to cindy's application." });
-});
+
